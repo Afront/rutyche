3fb6e01e36955225788a367800eb6292b4e318d6
Afront <3943720+Afront@users.noreply.github.com>
2020-05-18 17:25:11 +0900

    Refactor code with assertions

--- tests/self_methods.rb
+++ tests/self_methods.rb
@@ -2,22 +2,6 @@
 
 require 'pp'
 
-# module TypedMethods
-#   def self.extended(base)
-#     def self.bye
-#       p "Bye!"
-#     end
-
-#     def self.wow(a, b)
-#       p a,b
-#     end
-
-#     def self.add_integers(a, b)
-#       p a+b
-#     end
-#   end
-# end
-
 module TypedMethods
   module_function
 
@@ -30,8 +14,7 @@ module TypedMethods
   end
 
   def self.add_integers(a, b)
-    puts "Hi #{a + b}"
-    a + b
+    p a + b
   end
 
   def self.add_two_integers(a:, b:)
@@ -59,35 +42,30 @@ module TypeChecker
 
   TracePoint.trace(:call, :return) do |tp|
     # TODO: Use tp.defined_class to add support for multiple classes
-    # TODO: Checl tp.method_id vs tp.callee_id
-    if tp.method_id == :add_type_checking || @@tp_hash[tp.method_id].nil? || @@tp_hash[tp.method_id].empty?
-      next
-    end
+    # TODO: Check tp.method_id vs tp.callee_id
+    next if tp.method_id == :add_type_checking || @@tp_hash[tp.method_id].nil? || @@tp_hash[tp.method_id].empty?
 
     case tp.event
     when :call
       tp.parameters.each_with_index do |parameter, i|
         parameter_types = @@tp_hash[tp.method_id][:parameter_types]
         expected_parameter_type = parameter_types[parameter_types.class == Hash ? parameter[1] : i]
-        if expected_parameter_type == :untyped
-          next (p "hi #{parameter[1]} #{parameter_types} #{@@tp_hash[tp.method_id]}")
-        end
-        if expected_parameter_type != tp.binding.local_variable_get(parameter[1].to_s).class
-          raise TypeError, "Value (#{parameter[1]}) is not of class #{expected_parameter_type}"
-        end
+        next if expected_parameter_type == :untyped
+        raise TypeError, "Value (#{parameter[1]}) is not of class #{expected_parameter_type}" unless tp.binding.local_variable_get(parameter[1].to_s).is_a? expected_parameter_type
       end
     when :return
       puts tp.return_value
       next if @@tp_hash[tp.method_id][:return_type] == :untyped
-      if tp.return_value.class != @@tp_hash[tp.method_id][:return_type]
-        raise TypeError, "Value (#{tp.return_value}) is not of class #{@@tp_hash[tp.method_id][:return_type]}"
-      end
+      raise TypeError, "Value (#{tp.return_value}) is not of class #{@@tp_hash[tp.method_id][:return_type]}" unless tp.return_value.is_a? @@tp_hash[tp.method_id][:return_type]
     end
   end
 end
 
 module SelfMethods
   # extend TypedMethods
+  def self.assert result
+    raise unless result
+  end
 
   TypeChecker.add_type_checking(:bye)
   TypeChecker.add_type_checking(:add_integers, { a: Integer, b: Integer }, Integer)
@@ -95,11 +73,21 @@ module SelfMethods
   TypeChecker.add_type_checking(:concatenate_all_strings, [String, Array], String)
 
   # puts "hi"
-  TypedMethods.bye
-  TypedMethods.wow(5, 3)
-  TypedMethods.add_integers(2, 5)
-  p TypedMethods.add_two_integers(a: 2, b: 5)
-  TypedMethods.concatenate_all_strings('1', '2', '3')
+  assert TypedMethods.bye
+  assert TypedMethods.wow(5, 3)
+  assert TypedMethods.add_integers(2, 5)
+  assert p TypedMethods.add_two_integers(a: 2, b: 5)
+  assert TypedMethods.concatenate_all_strings('1', '2', '3')
 
-  # TypedMethods.add_integers("2", "5")
+  begin
+    TypedMethods.add_integers("2", "5")
+  rescue TypeError
+  else
+    raise "Test failed!"
+  end
+
+    TypedMethods.add_integers("2", 5)
+    TypedMethods.add_integers(2.3, 5.1)
+
+
 end

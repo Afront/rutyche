064c981aefa8ed30ea93bc2525e4ae075fa0621b
Afront <3943720+Afront@users.noreply.github.com>
2020-05-18 18:48:43 +0900

    Fix exception handling

--- tests/self_methods.rb
+++ tests/self_methods.rb
@@ -43,21 +43,31 @@ module TypeChecker
   TracePoint.trace(:call, :return) do |tp|
     # TODO: Use tp.defined_class to add support for multiple classes
     # TODO: Check tp.method_id vs tp.callee_id
-    next if tp.method_id == :add_type_checking || @@tp_hash[tp.method_id].nil? || @@tp_hash[tp.method_id].empty?
+    method_name = tp.method_id
+    type_error = nil
+    next if method_name == :add_type_checking || @@tp_hash[method_name].nil? || @@tp_hash[method_name].empty?
 
     case tp.event
     when :call
       tp.parameters.each_with_index do |parameter, i|
-        parameter_types = @@tp_hash[tp.method_id][:parameter_types]
+        parameter_types = @@tp_hash[method_name][:parameter_types]
         expected_parameter_type = parameter_types[parameter_types.class == Hash ? parameter[1] : i]
         next if expected_parameter_type == :untyped
-        raise TypeError, "Value (#{parameter[1]}) is not of class #{expected_parameter_type}" unless tp.binding.local_variable_get(parameter[1].to_s).is_a? expected_parameter_type
+        argument = tp.binding.local_variable_get(parameter[1].to_s)
+        type_error = TypeError.new "Argument `#{argument}' is not of class #{expected_parameter_type}" if !argument.is_a? expected_parameter_type
       end
     when :return
-      puts tp.return_value
+      return_value = tp.return_value
+      expected_return_type = @@tp_hash[method_name][:return_type]
+      puts return_value, "hi"
       next if @@tp_hash[tp.method_id][:return_type] == :untyped
-      raise TypeError, "Value (#{tp.return_value}) is not of class #{@@tp_hash[tp.method_id][:return_type]}" unless tp.return_value.is_a? @@tp_hash[tp.method_id][:return_type]
+
+      if !return_value.is_a? @@tp_hash[tp.method_id][:return_type]
+        type_error = TypeError.new "Return value (#{return_value}) is not of class #{@@tp_hash[tp.method_id][:return_type]}" unless type_error
+      end
     end
+
+    raise type_error unless type_error.nil?
   end
 end
 
@@ -67,27 +77,30 @@ module SelfMethods
     raise unless result
   end
 
+  def self.assert_error(container, method, *args)
+    begin
+     container.send(method, *args)
+    rescue TypeError
+    else
+     raise "Test failed!"
+    end
+  end
+
   TypeChecker.add_type_checking(:bye)
+  TypeChecker.add_type_checking(:wow, { a: Integer, b: Symbol }, Array)
   TypeChecker.add_type_checking(:add_integers, { a: Integer, b: Integer }, Integer)
   TypeChecker.add_type_checking(:add_two_integers, [Integer, Integer], Integer)
   TypeChecker.add_type_checking(:concatenate_all_strings, [String, Array], String)
 
   # puts "hi"
   assert TypedMethods.bye
-  assert TypedMethods.wow(5, 3)
+  assert TypedMethods.wow(5, :a)
   assert TypedMethods.add_integers(2, 5)
   assert p TypedMethods.add_two_integers(a: 2, b: 5)
   assert TypedMethods.concatenate_all_strings('1', '2', '3')
 
-  begin
-    TypedMethods.add_integers("2", "5")
-  rescue TypeError
-  else
-    raise "Test failed!"
-  end
-
-    TypedMethods.add_integers("2", 5)
-    TypedMethods.add_integers(2.3, 5.1)
-
-
+  assert_error(TypedMethods, :add_integers, "2", "5")
+  assert_error(TypedMethods, :add_integers, "2", 5)
+  assert_error(TypedMethods, :add_integers, 2.3, 5.1)
+  assert_error(TypedMethods, :add_integers, 2.3, 5)
 end

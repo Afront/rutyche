b24fdd28044d898fa9fbf7465fa4bf6e1adebd1a
Afront <3943720+Afront@users.noreply.github.com>
2020-05-26 23:58:45 +0900

    Add Test Methods

---
+++ test/test_methods.rb
@@ -0,0 +1,27 @@
+module TestMethods
+  module_function
+
+  def self.bye
+    'Bye!'
+  end
+
+  def self.wow(a, b)
+    [a, b]
+  end
+
+  def self.add_integers(a, b)
+    a + b
+  end
+
+  def self.add_two_integers(a:, b:)
+    a + b
+  end
+
+  def self.concatenate_all_strings(string, *args)
+    string + args.join
+  end
+
+  def self.puts(string, *args)
+    Kernel.puts string, *args
+  end
+end
